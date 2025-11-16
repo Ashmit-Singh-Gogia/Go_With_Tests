42f09a6e66b021debfc6e2f199c1c25e8a43ac47
Ashmit Singh Gogia <ashmit9955@gmail.com>
2025-11-16 22:26:14 +0530

    feat: "Area and its test"

--- Structs_Methods_Interfaces/shapes_test.go
+++ Structs_Methods_Interfaces/shapes_test.go
@@ -2,11 +2,20 @@ package main
 
 import "testing"
 
-func TestPerimete(t *testing.T) {
+func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f wnt %.2f", got, want)
 	}
 }
