6ae190bbc54f27054d76f0bd4fe0f355007c5002
Ashmit Singh Gogia <ashmit9955@gmail.com>
2025-11-19 11:47:27 +0530

    refactor: "Separated the got and want checks from the TestSearch"

--- Maps/dictionary_test.go
+++ Maps/dictionary_test.go
@@ -5,10 +5,16 @@ import "testing"
 func TestSearch(t *testing.T) {
 	t.Run("Searching in dictionary", func(t *testing.T) {
 		dictionary := map[string]string{"testKey": "This the test value"}
+
 		got := Search(dictionary, "testKey")
 		want := "This the test value"
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertStrings(t, got, want)
 	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
 }
