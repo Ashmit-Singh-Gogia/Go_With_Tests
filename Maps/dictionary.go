ac44a63ccb7e4ede767437a1b2a26a7411f05399
Ashmit Singh Gogia <ashmit9955@gmail.com>
2025-11-20 21:55:00 +0530

    feat: "Searching for a non existing word in dictionary"

--- Maps/dictionary.go
+++ Maps/dictionary.go
@@ -1,5 +1,13 @@
 package main
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(key string) (string, error) {
+	value, exists := d[key] // exists is a boolean which tells whether the value is present or not
+	if !exists {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return value, nil
 }
--- Maps/dictionary_test.go
+++ Maps/dictionary_test.go
@@ -4,11 +4,20 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	t.Run("Searching in dictionary", func(t *testing.T) {
-		dictionary := map[string]string{"testKey": "This the test value"}
+		dictionary := Dictionary{"testKey": "This the test value"}
 
-		got := Search(dictionary, "testKey")
+		got, _ := dictionary.Search("testKey")
 		want := "This the test value"
 		assertStrings(t, got, want)
+	})
+	t.Run("Searching a non existing key", func(t *testing.T) {
+		dictionary := Dictionary{}
+		_, err := dictionary.Search("unknownKey")
+
+		if err == nil {
+			t.Fatal("Expected to get an error")
+		}
+		assertStrings(t, err.Error(), "could not find the word you were looking for")
 	})
 }
 
